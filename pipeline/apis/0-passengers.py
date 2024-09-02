1a4359f6920ea5f6be901a5686d84e3a5b90e599
MrFejza <fejzaergis@gmail.com>
2024-09-02 18:52:20 +0200

    task 0

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#prototype
+# do python prototype 
+
+import requests
+
+def availableShips(passengerCount):
+    ships = []
+    base_url = 'https://swapi-api.hbtn.io/api/starships'
+    while base_url is not None:
+        response = requests.get(base_url)
+        data = response.json()
+        for ship in data['results']:
+            passengers = ship['passengers']
+            if passengers.isnumeric() and int(passengers) >= passengerCount:
+                ships.append(ship['name'])
+        base_url = data['next']
+    return ships
